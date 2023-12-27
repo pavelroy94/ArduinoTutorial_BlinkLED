a48dc6ee904073ee25acb51733886466ca33438a
Pavel Biswas <154968002+pavelroy94@users.noreply.github.com>
2023-12-27 23:24:26 +0530

    Update blink_led.ino

--- blink_led.ino
+++ blink_led.ino
@@ -1,8 +1,8 @@
-//Blink Led.
-//The purpose of writing this program is to blink a Led that is contolled via the digital pin 9.
+//Blink "LED".
+//The purpose of writing this program is to blink a "LED" that is contolled via the digital pin 9.
 
-int led = 9; //The digital pin number to which the positive wire is coonect to led via a resistor. The wire negative wire from the led is connected to ground on the arduino.
-int delayTime = 500; //This is the rate at which the led will turn on and off through the arduino. The delay is mentioned in milli seconds. In this case we are adding a delay of half a second. 
+int led = 9; //The digital pin number to which the positive wire is connect to "LED" via a resistor. The wire negative wire from the "LED" is connected to ground on the arduino.
+int delayTime = 500; //This is the rate at which the "LED" will turn on and off through the arduino. The delay is mentioned in milli seconds. In this case we are adding a delay of half a second. 
 
 void setup()
 {
